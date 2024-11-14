7c2095063140be3b35283d24d7e03308070bc0ae
Jameleh Asaad <jamelehasaad@gmail.com>
2024-11-15 01:42:47 +0300

    Completable Future in Use

--- src/DemoCompletableFuture.java
+++ src/DemoCompletableFuture.java
@@ -3,7 +3,13 @@ public class DemoCompletableFuture {
 
     public static void show() {
 
-       Quate quate1 = FlightService.getQuate("site1");
-        System.out.println(quate1);
+   var service = new  FlightService();
+          service.getQuate("site1").thenAccept(System.out::println);
+        try {
+            Thread.sleep(10_000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
--- src/FlightService.java
+++ src/FlightService.java
@@ -1,14 +1,18 @@
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
 
 public class FlightService {
 
-    public static Quate getQuate(String site){
+    public static CompletableFuture<Quate> getQuate(String site){
 
 
         System.out.println("Getting Quate from"+ site);
-        LongTask.simulate(3000);
-        Random random = new Random();
-        int price = random.nextInt(1000);
-        return new Quate(site, price);
+         var x =CompletableFuture.supplyAsync( () -> {
+            LongTask.simulate(3000);
+            Random random = new Random();
+            int price = random.nextInt(1000);
+            return new Quate(site, price);
+        });
+         return x;
     }
 }
